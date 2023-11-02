cd132cac4893bc857f1cd42688bd1d3ca67cd1d7
ScaredRabbit <scaredrabbitnl@gmail.com>
2023-11-02 08:56:11 +0100

    LootTables

--- src/main/java/net/scaredrabbit/tutorialmod/datagen/TutorialModDataGenerator.java
+++ src/main/java/net/scaredrabbit/tutorialmod/datagen/TutorialModDataGenerator.java
@@ -8,5 +8,7 @@ public class TutorialModDataGenerator implements DataGeneratorEntrypoint {
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 		pack.addProvider(ModRecipeProvider::new);
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
 	}
 }
